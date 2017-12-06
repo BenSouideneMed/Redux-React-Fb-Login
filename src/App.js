0b80f57717225ba38a71d5f04140c734aff25ffb
master <spatchola123@gmail.com>
2017-12-06 16:11:41 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+
+class App extends React.Component {
+
+
+  render() {
+
+      const responseFacebook = (response) => {
+          console.log(response);
+      }
+      const componentClicked = () => {
+          console.log("clicked");
+      }
+      return (
+      <div>
+          <h2>Welcome to Twin Minds</h2>
+        <FacebookLogin
+            appId="163127807768015"
+            autoLoad={true}
+            fields="name,email,picture"
+            onClick={componentClicked}
+            callback={responseFacebook} />
+      </div>
+    );
+  }
+}
+
+export default App;
