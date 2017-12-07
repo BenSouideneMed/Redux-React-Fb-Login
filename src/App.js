3384ca6b33e17ebd7cc351e820f48054e5a23299
master <spatchola123@gmail.com>
2017-12-07 15:59:04 +0100

    init

--- src/App.js
+++ src/App.js
@@ -3,43 +3,48 @@ import FacebookLogin from 'react-facebook-login';
 
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {name : "default"};
+    }
 
 
-  render() {
+    render() {
 
-      const responseFacebook = (response) => {
-          console.log(response);
-          console.log(response.name);
-          return (<div>{response.name}</div>)
-      }
-
-
+        const responseFacebook = (response) => {
+            console.log(response);
+            console.log(response.name);
+            this.setState({name :response.name});
+        }
 
 
-      const componentClicked = () => {
-          console.log("clicked");
-      }
-      return (
-      <div>
-          <h2>Welcome to Twin Minds</h2>
-        <FacebookLogin
-            appId="163127807768015"
-            autoLoad={true}
-            disableMobileRedirect={true}
-            fields="name,email,picture.width(800).height(800)"
-            scope="public_profile"
-            onClick={componentClicked}
-            callback={responseFacebook}
 
-        />
-          <div>
-              <h2>your name is : </h2>
-              <h2>{responseFacebook}</h2>
-          </div>
-      </div>
 
-    );
-  }
+        const componentClicked = () => {
+            console.log("clicked");
+        }
+
+        return (
+            <div>
+                <h2>Welcome to Twin Minds</h2>
+                <FacebookLogin
+                    appId="163127807768015"
+                    autoLoad={true}
+                    disableMobileRedirect={true}
+                    fields="name,email,picture.width(800).height(800)"
+                    scope="public_profile"
+                    onClick={() => {componentClicked}}
+                    callback={responseFacebook}
+
+                />
+                <div>
+                    <h2>your name is : </h2>
+                    <h2>{this.state.name}</h2>
+                </div>
+            </div>
+
+        );
+    }
 }
 
 export default App;
